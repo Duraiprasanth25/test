063746af34514e816df179c5842efca971b79841
Duraiprasanth25 <sdprasanth.5@gmail.com>
2024-04-10 16:00:48 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -1,15 +1,11 @@
-hungry= input("are you hungry?")
-if hungry=="yes":
+hungry = input("Are you hungry?")
+if hungry == "yes":
     print("eat samosa")
     print("eat burger")
     print("eat pizza")
-<<<<<<< HEAD
-    print('eat fries')
-=======
-    print('eat fries')
->>>>>>> thirsty
+    print("eat fries")  # Resolved conflict here
 else:
-    thirsty=input("are you thirsty?")
+    thirsty = input("Are you thirsty?")
     if thirsty == 'yes':
         print("drink water")
         print("drink soda")
