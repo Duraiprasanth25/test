8bd7553cec62064c8e1607900e8561195c304f84
Duraiprasanth25 <sdprasanth.5@gmail.com>
2024-04-10 16:03:47 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -5,7 +5,4 @@ if hungry == "yes":
     print("eat pizza")
     print("eat fries")  # Resolved conflict here
 else:
-    thirsty = input("Are you thirsty?")
-    if thirsty == 'yes':
-        print("drink water")
-        print("drink soda")
+   print("do home work")
